b60e9c7e4d27a70bbea8116a700cd2f6e549bf5a
Guilherme Carreiro <karreiro@gmail.com>
2017-04-02 12:59:15 -0300

    Add Coppelia copyright headers

--- src/main/java/coppelia/CharWA.java
+++ src/main/java/coppelia/CharWA.java
@@ -1,4 +1,20 @@
-// PUT_VREP_REMOTEAPI_COPYRIGHT_NOTICE_HERE
+/*
+ * Copyright 2006-2016 Coppelia Robotics GmbH. All rights reserved.
+ * marc@coppeliarobotics.com
+ * www.coppeliarobotics.com
+ *
+ * -------------------------------------------------------------------
+ * THIS FILE IS DISTRIBUTED "AS IS", WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY. THE USER WILL USE IT AT HIS/HER OWN RISK. THE ORIGINAL
+ * AUTHORS AND COPPELIA ROBOTICS GMBH WILL NOT BE LIABLE FOR DATA LOSS,
+ * DAMAGES, LOSS OF PROFITS OR ANY OTHER KIND OF LOSS WHILE USING OR
+ * MISUSING THIS SOFTWARE.
+ *
+ * You are free to use/modify/distribute this file for whatever purpose!
+ * -------------------------------------------------------------------
+ *
+ * This file was automatically created for V-REP release V3.3.2 on August 29th 2016
+ */
 
 package coppelia;
 
