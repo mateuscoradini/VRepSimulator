87038f35e594a707686b4aaaee54a8c5320d4db8
Mateus <Mateus>
2017-04-05 04:35:43 -0300

    Complete

--- src/main/java/br/com/pos/unicamp/vrep/PioneerRobot.java
+++ src/main/java/br/com/pos/unicamp/vrep/PioneerRobot.java
@@ -4,11 +4,9 @@ import java.util.Vector;
 
 import br.com.pos.unicamp.vrep.detectors.RequestSensorUltrassonic;
 import coppelia.BoolW;
-import coppelia.FloatW;
 import coppelia.FloatWA;
 import coppelia.IntW;
 import coppelia.remoteApi;
-import org.sonatype.plexus.components.sec.dispatcher.model.SettingsSecurity;
 
 public class PioneerRobot {
 
@@ -16,11 +14,19 @@ public class PioneerRobot {
     private static final String IP = "127.0.0.1";
     private static final int PORT = 25000;
 
-    private static final float NO_DETECT_DIST = 0.4f;
-    private static final float MAX_DETECTION_DIST = 0.3f;
-    private static final float MAX_DETECTION_DIST_VERTICAL = 0.4f;
+    //private static final float NO_DETECT_DIST = 0.7f;
+    private static final float MAX_DETECTION_DIST = 0.5f;
+    private static final float MAX_DETECTION_DIST_FRONT = 0.6f;
+    private static final float MIN_DETECTION_DIST = 0.45f;
+
+    private static final float MIN_DETECTION_DIST_TURN = 0.25f;
+    
+    private static final float MIN_COLISION_DETECTION_DIST_TURN = 0.12f;
+
+    // private static final float MAX_DETECTION_DIST_VERTICAL = 0.4f;
     private static final float INITIAL_VELOCITY = 0.5f;
 
+    private static final int SENSOR_1_NUMBER = 1;
     private static final int SENSOR_4_NUMBER = 3;
     private static final int SENSOR_5_NUMBER = 4;
 
@@ -28,10 +34,16 @@ public class PioneerRobot {
     private static final int SENSOR_6_NUMBER = 5;
     private static final int SENSOR_7_NUMBER = 6;
     private static final int SENSOR_8_NUMBER = 7;
-    private Vector<FloatW> braitenbergL = new Vector<FloatW>();
+    
+    
+    private static final int SENSOR_12_NUMBER = 11;
+    private static final int SENSOR_13_NUMBER = 12;
+    
+    
+    //private Vector<FloatW> braitenbergL = new Vector<FloatW>();
 
     // private float[] detect = new Vector<Float>();
-    private Vector<FloatW> braitenbergR = new Vector<FloatW>();
+    //private Vector<FloatW> braitenbergR = new Vector<FloatW>();
     private Vector<RequestSensorUltrassonic> proximitySensors = new Vector<RequestSensorUltrassonic>();
     private float[] detect = new float[16];
     private IntW leftMotor;
@@ -70,8 +82,8 @@ public class PioneerRobot {
     private void initializeSensors() {
         fillDetectSensors();
 
-        braitenbergL = fillBraitenbergL();
-        braitenbergR = fillBraitenbergR();
+        // braitenbergL = fillBraitenbergL();
+        // braitenbergR = fillBraitenbergR();
 
         // Ultrasonic sensors
         for (int i = 0; i < 16; i++) {
@@ -88,92 +100,92 @@ public class PioneerRobot {
         }
     }
 
-    private boolean isPlantDetected() {
-        return false;
-    }
-
-    //    private void initializeCamera() {
-    //        while(true) {
-    //            //
-    //        }
-    //    }
-
-    boolean reset = true;
+    boolean encontrouObstaculo = false;
+    boolean jaVoltou = false;
 
     public void actuation() throws InterruptedException {
         while (VREP_API.simxGetConnectionId(clientID) != -1) {
 
             float leftMotorVelocity = INITIAL_VELOCITY;
             float rightMotorVelocity = INITIAL_VELOCITY;
+            
+            
+            RequestSensorUltrassonic sensor3 = requestSensorUltrassonicDetectetion(SENSOR_3_NUMBER);
 
+            //Frontais
             RequestSensorUltrassonic sensor4 = requestSensorUltrassonicDetectetion(SENSOR_4_NUMBER);
             RequestSensorUltrassonic sensor5 = requestSensorUltrassonicDetectetion(SENSOR_5_NUMBER);
+
             RequestSensorUltrassonic sensor6 = requestSensorUltrassonicDetectetion(SENSOR_6_NUMBER);
+
+            //Direito
             RequestSensorUltrassonic sensor7 = requestSensorUltrassonicDetectetion(SENSOR_7_NUMBER);
             RequestSensorUltrassonic sensor8 = requestSensorUltrassonicDetectetion(SENSOR_8_NUMBER);
 
-            if ( (!checkDetectAndProximityFrontSensor(sensor4) || !checkDetectAndProximityFrontSensor(sensor5)) && checkDetectAndProximityFrontSensor(sensor6)  ) {
-                setTargetVelocity(leftMotor, INITIAL_VELOCITY);
-                setTargetVelocity(rightMotor, INITIAL_VELOCITY);
-            } else if ( (!checkDetectAndProximityFrontSensor(sensor6) || !checkDetectAndProximityFrontSensor(sensor7)) && !checkDetectAndProximityFrontSensor(sensor8)) {
+            //Tem não tem algo na frente
+            if (seNaoHaObstaculoAFrente(sensor4, sensor5, sensor6)) {
 
-                float velocityLeft = INITIAL_VELOCITY - 0.4f;
-                float velocityRigt = INITIAL_VELOCITY - 0.1f;
-                setTargetVelocity(leftMotor, velocityLeft);
-                setTargetVelocity(rightMotor, velocityRigt);
-            } else if (checkDetectAndProximityFrontSensor(sensor6) || checkDetectAndProximityFrontSensor(sensor8)) {
+                if (seHaAlgoDetectadoEntreDireita(sensor7) && seHaAlgoDetectadoEntreDireita(sensor8)) {
+                    System.out.println("1");
+                    setTargetVelocity(leftMotor, leftMotorVelocity);
+                    setTargetVelocity(rightMotor, rightMotorVelocity);
+
+                }
+
+                if (distMinDireitaFollow(sensor7, sensor8)) {
+                    System.out.println("2");
+                    setTargetVelocity(leftMotor, 0.5f);
+                    setTargetVelocity(rightMotor, 0f);
+                    Thread.sleep(1500);
+                    setTargetVelocity(leftMotor, 0.5f);
+                    setTargetVelocity(rightMotor, 0.5f);
+                    Thread.sleep(1500);
+                }
+
+                if (distMaxDireitaFollow(sensor7, sensor8) && encontrouObstaculo) {
+                    System.out.println("3");
+                    setTargetVelocity(leftMotor, 0.5f);
+                    setTargetVelocity(rightMotor, 0f);
+                    Thread.sleep(1500);
+                    setTargetVelocity(leftMotor, 0.5f);
+                    setTargetVelocity(rightMotor, 0.5f);
+                    Thread.sleep(1000);
+                }
+
+                System.out.println("|||||||||||||||||||||||||||||||||||||");
+
+            } else {
                 
-                float velocityLeft = INITIAL_VELOCITY - 0.6f;
-                float velocityRigt = INITIAL_VELOCITY - 0.2f;
-                setTargetVelocity(leftMotor, velocityLeft);
-                setTargetVelocity(rightMotor, velocityRigt);
+                //TODO Construir inteligencia para fazer rollback da sala que entrou
+
+                if (distMinDireita(sensor7)) {
+                    System.out.println("0");
+                    
+                    //NOTA: É mais facil o robo aprender a contornar o objeto do que controlar a precisão dos sensores.
+                    virar90praEsquerdaRapida();                   
+                    encontrouObstaculo = true;
+                    
+//                    RequestSensorUltrassonic sensor12 = requestSensorUltrassonicDetectetion(SENSOR_12_NUMBER);
+//                    RequestSensorUltrassonic sensor13 = requestSensorUltrassonicDetectetion(SENSOR_13_NUMBER);
+//                    if(!distMinCentral(sensor12) || !distMinCentral(sensor13) && (!(distMinCentral(sensor4) || distMinCentral(sensor3) || distMinCentral(sensor5)))){
+//                        voltar();
+//                    }
+                }
                 
-                //TODO Mapear os sonares 8 como linha seguidora a partir desse ponto que encontrou parede
-                
-//                if(!checkDetectAndProximityFrontSensor(sensor7) || checkDetectSensor(sensor8)){
-//                    
-//                    
-//                    velocityLeft = INITIAL_VELOCITY;
-//                    velocityRigt = INITIAL_VELOCITY;
-//                    setTargetVelocity(leftMotor, velocityLeft);
-//                    setTargetVelocity(rightMotor, velocityRigt);
-//                    
-//                    
+//                //Ele se auto-corrige em 0.1 ms pra tras, caso ele nao consiga dar a volta na sala, a sala é um ambiente não exploravel por limitação de detecao dos sensores (programacao)
+//                if((distMinCentral(sensor4) || distMinCentral(sensor3) || distMinCentral(sensor5)) && !distMinDireita(sensor7) && !jaVoltou){
+//                    voltar();
+//                    jaVoltou = true;                   
+//                }else{
+//                    RequestSensorUltrassonic sensor12 = requestSensorUltrassonicDetectetion(SENSOR_12_NUMBER);
+//                    RequestSensorUltrassonic sensor13 = requestSensorUltrassonicDetectetion(SENSOR_13_NUMBER);
+//                    if(distMinCentral(sensor12) || distMinCentral(sensor13)){
+//                        setTargetVelocity(leftMotor, 0.1f);
+//                        setTargetVelocity(rightMotor, 0.1f);
+//                    }
 //                    
 //                }
-                
-                
-
-                
             }
-
-            //            RequestSensorUltrassonic sensor8 = requestSensorUltrassonicDetectetion(7);
-            //
-            //            //Encontrar a primeiro obstaculo (parede)
-            //            if (reset && !checkDetectAndProximitySensor(sensor8)) {
-            //                virar90praDireita(reset);
-            //                Thread.sleep(3600);
-            //                parar();               
-            //            }
-
-            //
-            //            RequestSensorUltrassonic sensor4 = requestSensorUltrassonicDetectetion(3);
-            //            //Ler a distancia do sensor 4 e 5
-            //            if (checkDetectAndProximityFrontSensor(sensor4)) {
-            //                wallFollowing();
-            //                Thread.sleep(1000);
-            //            } else {
-            //
-            //                setTargetVelocity(leftMotor, 0.0f);
-            //                setTargetVelocity(rightMotor, 0.0f);
-            //                reset = true;
-            //                virar90praEsquerda(reset);
-            //                Thread.sleep(3600);
-            //                parar();
-            //                reset = false;
-            //                
-            //
-            //            }
 
         }
 
@@ -181,90 +193,116 @@ public class PioneerRobot {
         System.out.println("Connection closed.");
     }
 
-    //    private boolean checkDetectAndProximitySensor(RequestSensorUltrassonic sensor) {
-    //        boolean detected = sensor.getState().getValue();
-    //        float dist = sensor.getCoord().getArray()[2];
-    //
-    //        if (detected && dist < NO_DETECT_DIST) {
-    //            return true;
-    //
-    //        }
-    //        return false;
-    //    }
-    
-    private boolean checkDetectSensor(RequestSensorUltrassonic sensor) {
-        boolean detected = sensor.getState().getValue();
-        //float dist = sensor.getCoord().getArray()[2];
-
-        if (detected) {
-            return true;
-        }
-        return false;
-    }
-
-    private void wallFollowing() {
-        RequestSensorUltrassonic sensor4 = requestSensorUltrassonicDetectetion(3);
-        if (checkDetectAndProximityFrontSensor(sensor4)) {
-            setTargetVelocity(leftMotor, 0.2f);
-            setTargetVelocity(rightMotor, 0.2f);
-        }
-
-        RequestSensorUltrassonic sensor5 = requestSensorUltrassonicDetectetion(4);
-        if (checkDetectAndProximityFrontSensor(sensor5)) {
-            setTargetVelocity(leftMotor, 0.4f);
-            setTargetVelocity(rightMotor, 0.4f);
-
-        }
-
-    }
-
-    private void virar90praDireita(boolean reset) {
-        if (reset) {
-            setTargetVelocity(leftMotor, 1f);
-            setTargetVelocity(rightMotor, -1f);
-        }
-    }
-
-    private void virar90praEsquerda(boolean reset) {
-
-        if (reset) {
-            setTargetVelocity(leftMotor, -1f);
-            setTargetVelocity(rightMotor, 1f);
-        }
-
-    }
-
-    private void parar() {
-        setTargetVelocity(leftMotor, 0f);
-        setTargetVelocity(rightMotor, 0f);
-        reset = false;
-    }
-
-    private boolean checkDetectAndProximitySensor(RequestSensorUltrassonic sensor) {
-        boolean detected = sensor.getState().getValue();
+    private boolean distMinCentral(RequestSensorUltrassonic sensor) {
         float dist = sensor.getCoord().getArray()[2];
-
-        if (detected && dist < NO_DETECT_DIST) {
-            return true;
-        }
-        return false;
-    }
-
-    private boolean checkDetectAndProximityFrontSensor(RequestSensorUltrassonic sensor) {
         boolean detected = sensor.getState().getValue();
-        float dist = sensor.getCoord().getArray()[2];
-
-        if (detected && dist > NO_DETECT_DIST) {
+        if (detected && (dist <= MIN_COLISION_DETECTION_DIST_TURN)) {
+            System.out.println("Detectado distancia de colisao:" + sensor.getSensorName());
             return true;
         }
         return false;
     }
 
+    private boolean noDetectedRight(RequestSensorUltrassonic sensor8) {
+
+        boolean detected8 = sensor8.getState().getValue();
+        float dist8 = sensor8.getCoord().getArray()[2];
+
+        if (detected8) {
+            if (dist8 > MIN_DETECTION_DIST) {
+                return true;
+            }
+            return false;
+
+        }
+        return false;
+
+    }
+
+    private boolean distMaxDireitaFollow(RequestSensorUltrassonic sensor7, RequestSensorUltrassonic sensor8) {
+
+        boolean detected7 = sensor7.getState().getValue();
+        boolean detected8 = sensor8.getState().getValue();
+
+        float dist8 = sensor8.getCoord().getArray()[2];
+        float dist7 = sensor7.getCoord().getArray()[2];
+        
+        //Caso queira uma distancia menor de seguir parede, temos que calibrar o tempo de resposta do motores pra ser mais devagar para fazer as curvas sem penalizacao
+        float minimaDistanciaTeste = MIN_DETECTION_DIST - 0.1f;
+
+        if ((detected7 && dist7 >= minimaDistanciaTeste) && (detected8 && dist8 >= minimaDistanciaTeste)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private boolean distMinDireitaFollow(RequestSensorUltrassonic sensor7, RequestSensorUltrassonic sensor8) {
+        float dist7 = sensor7.getCoord().getArray()[2];
+        float dist8 = sensor8.getCoord().getArray()[2];
+        boolean detected7 = sensor7.getState().getValue();
+        
+        //float minimaDistanciaTeste = MIN_DETECTION_DIST - 0.1f;
+        
+        if ((!detected7 && dist7 <= MIN_DETECTION_DIST)) {
+            return true;
+        }
+        return false;
+
+    }    
+
+    private boolean distMinDireita(RequestSensorUltrassonic sensor) {
+        float dist = sensor.getCoord().getArray()[2];
+        boolean detected = sensor.getState().getValue();
+        if (detected && (dist <= MIN_DETECTION_DIST)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private boolean seHaAlgoDetectadoEntreDireita(RequestSensorUltrassonic sensor) {
+        float dist = sensor.getCoord().getArray()[2];
+        boolean detected = sensor.getState().getValue();
+        if (detected && (dist >= MAX_DETECTION_DIST_FRONT)) {
+            return true;
+        }
+        return false;
+
+    }
+
+    private boolean seNaoHaObstaculoAFrente(RequestSensorUltrassonic sensor4, RequestSensorUltrassonic sensor5, RequestSensorUltrassonic sensor6) {
+        return !seCaminhoAFrente(sensor4) && !seCaminhoAFrente(sensor5) && !seCaminhoAFrente(sensor6);
+    }
+
+    private boolean seCaminhoAFrente(RequestSensorUltrassonic sensor) {
+        float dist = sensor.getCoord().getArray()[2];
+        boolean detected = sensor.getState().getValue();
+        if (detected && dist < MAX_DETECTION_DIST) {
+            System.out.println("Há algo na frente:" + sensor.getSensorName());
+            return true;
+        }
+        System.out.println("Não Há algo na frente:" + sensor.getSensorName());
+        return false;
+    }
+
+    private void virar90praEsquerdaRapida() {
+        setTargetVelocity(leftMotor, 0.1f);
+        setTargetVelocity(rightMotor, 0.7f);
+    }
     
+    private void voltar() {
+        setTargetVelocity(leftMotor, -0.2f);
+        setTargetVelocity(rightMotor, -0.1f);
+    }
+
+    private void virar90praDireita() {
+        setTargetVelocity(leftMotor, 0.8f);
+        setTargetVelocity(rightMotor, -0.2f);
+    }
 
     private void setTargetVelocity(IntW motorHandle, float velocity) {
         VREP_API.simxSetJointTargetVelocity(clientID, motorHandle.getValue(), velocity, VREP_API.simx_opmode_streaming);
-
     }
 
     private RequestSensorUltrassonic requestSensorUltrassonicDetectetion(int sensorNumber) {
@@ -310,60 +348,4 @@ public class PioneerRobot {
         VREP_API.simxFinish(clientID);
     }
 
-    private Vector<FloatW> fillBraitenbergL() {
-        Vector<FloatW> vector = new Vector<FloatW>();
-        final FloatW w1 = new FloatW(-0.2f);
-        final FloatW w2 = new FloatW(-0.4f);
-        final FloatW w3 = new FloatW(-0.6f);
-        final FloatW w4 = new FloatW(-0.8f);
-        final FloatW w5 = new FloatW(-1f);
-        final FloatW w6 = new FloatW(-1.2f);
-        final FloatW w7 = new FloatW(-1.4f);
-        final FloatW w8 = new FloatW(-1.6f);
-        final FloatW w0 = new FloatW(-0f);
-
-        vector.add(w1);
-        vector.add(w2);
-        vector.add(w3);
-        vector.add(w4);
-        vector.add(w5);
-        vector.add(w6);
-        vector.add(w7);
-        vector.add(w8);
-
-        for (int i = 0; i < 8; i++) {
-            vector.addElement(w0);
-        }
-
-        return vector;
-    }
-
-    private Vector<FloatW> fillBraitenbergR() {
-        final Vector<FloatW> vector = new Vector<FloatW>();
-
-        final FloatW w0 = new FloatW(-0f);
-        final FloatW w1 = new FloatW(-0.2f);
-        final FloatW w2 = new FloatW(-0.4f);
-        final FloatW w3 = new FloatW(-0.6f);
-        final FloatW w4 = new FloatW(-0.8f);
-        final FloatW w5 = new FloatW(-1f);
-        final FloatW w6 = new FloatW(-1.2f);
-        final FloatW w7 = new FloatW(-1.4f);
-        final FloatW w8 = new FloatW(-1.6f);
-
-        vector.add(w8);
-        vector.add(w7);
-        vector.add(w6);
-        vector.add(w5);
-        vector.add(w4);
-        vector.add(w3);
-        vector.add(w2);
-        vector.add(w1);
-
-        for (int i = 0; i < 8; i++) {
-            vector.addElement(w0);
-        }
-
-        return vector;
-    }
 }
